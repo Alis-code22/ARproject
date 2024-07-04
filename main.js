7bc20e64324f8588dba31ee605db37acaf2d0c52
alis <alis@test.it>
2024-07-04 23:45:57 +0200

    welcome screen

--- main.js
+++ main.js
@@ -3,22 +3,17 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+let video = null;
+let app = null;
+
 // webcam connection using WebRTC
-window.onload = function(){
-    const video = document.getElementById("myvideo");	
-    video.onloadedmetadata = start_processing;
-    const constraints = {audio: false, video: {facingMode: 'environment'}};
-    navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => video.srcObject = stream )
-    .catch((err) => {
-        alert(err.name + ": " + err.message);	
-        video.src = "marker.webm";
-    });
+window.onload = function () {
+	document.getElementById('start_button').addEventListener('click', start );
 }
 
 let containers = {};
 
-const models = { 
+const models = {
 	//0: 'retro_cartoon_car.glb',
 	4: { file: 'Lamp.glb', scale: .02 },
 	11: { file: 'Pink_pc.glb', scale: .03 },
@@ -32,18 +27,18 @@ const loader = new GLTFLoader();
 let objects = {};
 const getModel = async (id) => {
 	const obj = objects[id];
-	if(obj == 'loading')
+	if (obj == 'loading')
 		return null;
 
-	if(obj) return obj; // loaded
+	if (obj) return obj; // loaded
 
 	const m = models[id];
-	if(!m) {
+	if (!m) {
 		return null;
 	}
 
 	const path = 'models/' + m.file;
-	console.log( 'loading model id %s from %s', id, path );
+	console.log('loading model id %s from %s', id, path);
 	objects[id] = 'loading';
 	return new Promise((resolve, reject) => {
 		loader.load(path, model => {
@@ -53,17 +48,17 @@ const getModel = async (id) => {
 		});
 	})
 }
-let dbg = true;
+
 const get_container = async (id) => {
 	let c = containers[id];
-	if(c) {
+	if (c) {
 		c.lastdetectiontime = performance.now();
 		c.first_detection = false;
 		return c;
 	}
 
 	let model = await getModel(id);
-	if(!model) {
+	if (!model) {
 		return null;
 	}
 
@@ -71,8 +66,8 @@ const get_container = async (id) => {
 	container.matrixAutoUpdate = false;
 	container.add(model.scene);
 
-	const light = new THREE.AmbientLight(0xffffff,2);
-	container.add(light);		
+	const light = new THREE.AmbientLight(0xffffff, 2);
+	container.add(light);
 	// const axesHelper = new THREE.AxesHelper(1);
 	// container.add(axesHelper);
 	let k = { container: container, lastdetectiontime: performance.now(), first_detection: true };
@@ -81,24 +76,22 @@ const get_container = async (id) => {
 };
 
 // fix the marker matrix to compensate Y-up models
-function fixMatrix(three_mat, m){
-    three_mat.set(
-        m[0], m[8], -m[4], m[12],
-        m[1], m[9], -m[5], m[13],
-        m[2], m[10], -m[6], m[14],
-        m[3], m[11], -m[7], m[15]
-    );
+function fixMatrix(three_mat, m) {
+	three_mat.set(
+		m[0], m[8], -m[4], m[12],
+		m[1], m[9], -m[5], m[13],
+		m[2], m[10], -m[6], m[14],
+		m[3], m[11], -m[7], m[15]
+	);
 }
 
-function start_processing(){
-    // canvas & video
-    const video = document.getElementById("myvideo");
-    const canvas = document.getElementById("mycanvas");
-    // canvas.width = video.videoWidth;
-    // canvas.height = video.videoHeight;
+function start_processing() {
+	// canvas & video
+	const canvas = document.getElementById("mycanvas");
+	// canvas.width = video.videoWidth;
+	// canvas.height = video.videoHeight;
 
-	const c = document.getElementsByClassName('container')[0];
-	if( c.clientHeight > c.clientWidth ) {
+	if (app.clientHeight > app.clientWidth) {
 		canvas.style.width = '100%';
 		canvas.style.height = `min(100%, 100vw * ${video.videoHeight} / ${video.videoWidth})`;
 	}
@@ -108,51 +101,50 @@ function start_processing(){
 	}
 
 	video.width = video.height = 0;
-	
+
 	// canvas.style.aspectRatio = video.style.aspectRatio;
 
-    // three.js
-    const renderer = new THREE.WebGLRenderer( { canvas: canvas } );
-    const scene = new THREE.Scene();
-    const camera = new THREE.Camera();
-    scene.add(camera);
+	// three.js
+	const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+	const scene = new THREE.Scene();
+	const camera = new THREE.Camera();
+	scene.add(camera);
 
-    // background
-    const bgtexture = new THREE.VideoTexture( video );
-    bgtexture.colorSpace = THREE.SRGBColorSpace;
-    scene.background = bgtexture;
-	
-    // container + object
+	// background
+	const bgtexture = new THREE.VideoTexture(video);
+	bgtexture.colorSpace = THREE.SRGBColorSpace;
+	scene.background = bgtexture;
 
-    // jsartoolkit
-    let arLoaded = false;
-    let lastdetectiontime = 0;
-    const arController = new ARController(video, 'camera_para.dat');
-    arController.onload = () => {
-		// debugger
-        camera.projectionMatrix.fromArray( arController.getCameraMatrix() );
-        arController.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);
-        // arController.setMatrixCodeType(artoolkit.AR_MATRIX_CODE_3x3);
-        arController.addEventListener('getMarker', ev => {
-            if(ev.data.marker.idMatrix != -1){
+	// container + object
+
+	// jsartoolkit
+	let arLoaded = false;
+	let lastdetectiontime = 0;
+	const arController = new ARController(video, 'camera_para.dat');
+	arController.onload = () => {
+		camera.projectionMatrix.fromArray(arController.getCameraMatrix());
+		arController.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);
+		// arController.setMatrixCodeType(artoolkit.AR_MATRIX_CODE_3x3);
+		arController.addEventListener('getMarker', ev => {
+			if (ev.data.marker.idMatrix != -1) {
 				// console.log( "Marker Index: %s, Matrix id: %s", ev.data.index, ev.data.marker?.idMatrix );
-				get_container(ev.data.marker.idMatrix).then( c => {
-					if(!c) return;
-					fixMatrix(c.container.matrix, ev.data.matrixGL_RH );
-					if( c.first_detection )
+				get_container(ev.data.marker.idMatrix).then(c => {
+					if (!c) return;
+					fixMatrix(c.container.matrix, ev.data.matrixGL_RH);
+					if (c.first_detection)
 						scene.add(c.container);
 
 				})
-            }
-        });
-        arLoaded = true;
-    }
+			}
+		});
+		arLoaded = true;
+	}
 
-    // render loop
-    function renderloop() {
-        requestAnimationFrame( renderloop );
-        if(arLoaded)
-            arController.process(video);
+	// render loop
+	function renderloop() {
+		requestAnimationFrame(renderloop);
+		if (arLoaded)
+			arController.process(video);
 
 		const width = canvas.clientWidth;
 		const height = canvas.clientHeight;
@@ -166,18 +158,38 @@ function start_processing(){
 
 		const now = performance.now();
 		let ixs = Object.keys(containers);
-		// debugger
-		for( let i = 0; i < ixs.length; i++ ) {
+		for (let i = 0; i < ixs.length; i++) {
 			const k = ixs[i];
 			let c = containers[k];
-			if( now - c.lastdetectiontime < 100 )
+			if (now - c.lastdetectiontime < 100)
 				c.container.visible = true;
 			else
-			    c.container.visible = false;
+				c.container.visible = false;
 		}
-        // if(performance.now()-lastdetectiontime < 100)
-        renderer.render( scene, camera );
-    }
-    renderloop();
+		// if(performance.now()-lastdetectiontime < 100)
+		renderer.render(scene, camera);
+	}
+	renderloop();
 }
 
+export function start() {
+	const wel = document.getElementById('welcome');
+	wel.classList.add('hidden');
+	wel.classList.remove('cover_all');
+	
+	app = document.getElementById('app');
+	app.classList.add('cover_all');
+	app.classList.remove('hidden');
+	
+
+	video = document.getElementById("myvideo");
+	video.onloadedmetadata = start_processing;
+	const constraints = { audio: false, video: { facingMode: 'environment' } };
+	
+	navigator.mediaDevices.getUserMedia(constraints)
+		.then((stream) => video.srcObject = stream)
+		.catch((err) => {
+			alert(err.name + ": " + err.message);
+			// video.src = "marker.webm";
+		});	
+}
